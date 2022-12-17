0064b17bd65dd3f3f947a13162d92fc270e2ec69
Ro <roshigen@gmail.com>
2022-12-17 20:42:04 +0900

    123

---
+++ TestIfElse.java
@@ -0,0 +1,11 @@
+public class TestIfElse {
+	public static void main(String[] args) {
+		int x = 300;
+		if(x < 20) {
+			System.out.println("这是if语句");
+		} else {
+			System.out.println("这是else语句");
+		}
+	}
+	
+}
---
+++ TestSwitch.java
@@ -0,0 +1,21 @@
+public class TestSwitch {
+	public static void main(String[] args) {
+		int A = 10;
+		
+		switch（);
+		{
+		case '10' :
+			System.out.println("1数字");
+			break;
+		case '20' :
+			System.out.println("2数字");
+			break;
+		case '35' :
+		case '40' :
+			System.out.println("33数字");
+			break;
+		}
+		default:
+			System.out.printle("超过数字");
+	}
+}
