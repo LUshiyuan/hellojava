c3efc384003c0b747ea2b305320704bb14a98eeb
Ro <roshigen@gmail.com>
2022-12-18 22:17:02 +0900

    comment

---
+++ Commen.java
@@ -0,0 +1,12 @@
+public class Commen {
+	public static void main(String[] args) {
+		/*定义变量
+		 两个数相加*/
+		int a = 10;
+		int b = 20;
+		//求和
+		int sum = a + b;
+		//输出结果
+		System.out.println("结果是=" + sum);
+	}
+}
