5a0b599d6bcdc21285386fc3a1f357d5472872cc
Ro <roshigen@gmail.com>
2022-12-18 21:22:45 +0900

    changechar

---
+++ ChangeChar.java
@@ -0,0 +1,17 @@
+public class ChangeChar {
+	//编写一个main方法
+	public static void main(String[] args) {
+//		\t :一个制表位 实现对其功能固定间隔
+		System.out.println("天津\t上海\t广州");
+//		\n :换行符
+		System.out.println("天津\n上海\n广州");
+//		\\ :一个\
+		System.out.println("天津\\上海\\广州");
+//		\" :一个“
+		System.out.println("我说\"要好好学习\"");
+//		\' :一个‘
+		System.out.println("");
+//		\r :一个回车
+		System.out.println("好好学习\r学习\nhhh");
+	}
+}
