10ae282c75ee153f680b362b2b62f39dc1b68441
Ro <roshigen@gmail.com>
2022-12-18 20:03:09 +0900

    2

---
+++ HalloJava.java
@@ -0,0 +1,10 @@
+public class HalloJava {
+	public static void main(String[] args) {
+		System.out.println("Hello Java!");
+		System.out.println("You were born in 1995");
+		System.out.println("You were created by James Gosling");
+		System.out.println("You are a fun language!");
+		
+	}
+	
+}
---
+++ HelloJack.java
@@ -0,0 +1,6 @@
+public class HelloJack {
+	public static void main(String[] args) {
+		System.out.println("HelloJack!");
+		
+	}
+}
